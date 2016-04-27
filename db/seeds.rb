2af59309bac4b7df4ff5d57e67ffbc5967453537
Ann Novakowski <annhnova@gmail.com>
2016-04-27 12:33:29 -0500

    Checkpoint 30 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'random_data'
+
+# Create Posts
+50.times do
+  Post.create!( # Adding a ! instructs the method to raise an error if there's a problem with the data we're seeding.
+  title: RandomData.random_sentence,
+  body: RandomData.random_paragraph
+  )
+end
+posts = Post.all
+
+# Create Comments
+100.times do # we call times on an Integer (a number object). This will run a given block the specified number of times, which is 100 in this case. The end result of calling times is similar to that of a loop, but in this use-case it is easier to read
+  Comment.create!(
+  post: posts.sample,
+  body: RandomData.random_paragraph
+  )
+end
+
+puts "Seed finished"
+puts "#{Post.count} posts created"
+puts "#{Comment.count} comments created"
