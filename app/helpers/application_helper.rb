4c386cc2046770b4103ae6174fce8564485d8215
Ann Novakowski <annhnova@gmail.com>
2016-06-25 15:44:00 -0400

    Checkpoint 35: completed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def form_group_tag(errors, &block)
+    # The & turns the block into a Proc, which is a block that can be used as a variable
+    css_class = 'form-group'
+    css_class << ' has-error' if errors.any?
+
+    content_tag :div, capture(&block), class: css_class
 end
