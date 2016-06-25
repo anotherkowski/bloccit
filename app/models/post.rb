b5bc8dca0daeac633d0caffd7b7170dd4ee40c5a
Ann Novakowski <annhnova@gmail.com>
2016-06-25 15:38:51 -0400

    Checkpoint 35: in progress

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,9 @@ class Post < ActiveRecord::Base
   belongs_to :topic
   has_many :comments, dependent: :destroy
 
+  validates :title, length: { minimum: 5 }, presence: true
+  validates :body, length: { minimum: 20 }, presence: true
+  validates :topic, presence: true
   # def Post.write_spam!
   #   Post.all.each_with_index do | post, index |
   #     if index % 5 == 0 then
