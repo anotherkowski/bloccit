dce4da6693d58f3384eec7775c457189d3670bd2
Ann Novakowski <annhnova@gmail.com>
2016-06-25 15:23:18 -0400

    Checkpoint 34: completed

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,4 +2,53 @@ class TopicsController < ApplicationController
   def index
     @topics = Topic.all
   end
+  def show
+    @topic = Topic.find(params[:id]) #display the topic that matches the id passed in URL params
+  end
+  def new
+    @topic = Topic.new #create a new topic
+  end
+  def create
+    @topic = Topic.new
+    @topic.name = params[:topic][:name]
+    @topic.description = params[:topic][:description]
+    @topic.public = params[:topic][:public]
+
+    if @topic.save
+      flash[:notice] = "Topic was saved successfully."
+      redirect_to @topic
+    else
+      flash.now[:alert] = "Error creating topic. Please try again."
+      render :new
+    end
+  end
+  def edit
+    @topic = Topic.find(params[:id])
+  end
+  def update
+  @topic = Topic.find(params[:id])
+
+  @topic.name = params[:topic][:name]
+  @topic.description = params[:topic][:description]
+  @topic.public = params[:topic][:public]
+
+  if @topic.save
+     flash[:notice] = "Topic was updated successfully."
+    redirect_to @topic
+  else
+    flash.now[:alert] = "Error saving topic. Please try again."
+    render :edit
+  end
+end
+   def destroy
+     @topic = Topic.find(params[:id])
+
+     if @topic.destroy
+       flash[:notice] = "\"#{@topic.name}\" was deleted successfully."
+       redirect_to action: :index #redirect_to action: :index is the same as redirect_to topics_path because topics_path routes to the index action per Rails' resourceful routing.
+     else
+       flash.now[:alert] = "There was an error deleting the topic."
+       render :show
+     end
+   end
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,4 @@
 class Topic < ActiveRecord::Base
-  has_many :posts
+  has_many :posts, dependent: :destroy
+  #note: Because comments already depend on posts, they will also be deleted when a topic is deleted.
 end
