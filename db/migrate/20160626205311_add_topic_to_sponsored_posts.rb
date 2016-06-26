2fad439eca750319c5c272e90fa2c4662233051b
Ann Novakowski <annhnova@gmail.com>
2016-06-26 16:02:48 -0500

    Migrate association AddTopicToSponsoredPosts

--- db/migrate/20160626205311_add_topic_to_sponsored_posts.rb
+++ db/migrate/20160626205311_add_topic_to_sponsored_posts.rb
@@ -1,6 +1,6 @@
 class AddTopicToSponsoredPosts < ActiveRecord::Migration
   def change
-    add_column :sponsored_posts, :topic_id, :integer
-    add_index :sponsored_posts, :topic_id
+    add_column :sponsored_posts, :topic_id_, :integer
+    add_index :sponsored_posts, :topic_id_
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160626202738) do
+ActiveRecord::Schema.define(version: 20160626205311) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -55,9 +55,11 @@ ActiveRecord::Schema.define(version: 20160626202738) do
     t.integer  "topic_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "topic_id_"
   end
 
   add_index "sponsored_posts", ["topic_id"], name: "index_sponsored_posts_on_topic_id"
+  add_index "sponsored_posts", ["topic_id_"], name: "index_sponsored_posts_on_topic_id_"
 
   create_table "topics", force: :cascade do |t|
     t.string   "name"
