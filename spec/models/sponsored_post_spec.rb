7ed64bd2ff1b13753591d369fd5d040db9100bd6
Ann Novakowski <annhnova@gmail.com>
2016-06-26 15:45:27 -0500

    Add shoulda associations specs for SponsoredPost and Topic

--- spec/models/sponsored_post_spec.rb
+++ spec/models/sponsored_post_spec.rb
@@ -1,12 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe SponsoredPost, type: :model do
+  # Attributes for parent topic
+  let(:name) {RandomData.random_sentence}
+  let(:description) {RandomData.random_paragraph}
+  let(:topic) {Topic.create!(name: name, description: description)}
+  # Attributes for sponsored post
   let(:title) {RandomData.random_sentence}
   let(:body) {RandomData.random_paragraph}
   let(:price) {rand(100)}
-  let(:sponsoredpost) {SponsoredPost.create!(title: title, body: body, price: price)}
-  # let(:topic)
-  # let(:sponsoredpost) {Topic.create!()}
+  let(:sponsoredpost) {topic.sponsoredposts.create!(title: title, body: body, price: price)}
+  # Expect test sponsored post to have a parent topic
+  it {is_expected.to belong_to(:topic)}
+
   describe "attributes" do
     it "has title, description, price attributes" do
       expect(sponsoredpost).to have_attributes(title: title, body: body, price: price)
