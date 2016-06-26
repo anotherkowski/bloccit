4de062f0c45b60265e1e53a0e4b25aec0ae5feed
Ann Novakowski <annhnova@gmail.com>
2016-06-26 15:35:32 -0500

    Create attributes spec for SponsoredPost

--- spec/models/sponsored_post_spec.rb
+++ spec/models/sponsored_post_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe SponsoredPost, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:title) {RandomData.random_sentence}
+  let(:body) {RandomData.random_paragraph}
+  let(:price) {rand(100)}
+  let(:sponsoredpost) {SponsoredPost.create!(title: title, body: body, price: price)}
+  # let(:topic)
+  # let(:sponsoredpost) {Topic.create!()}
+  describe "attributes" do
+    it "has title, description, price attributes" do
+      expect(sponsoredpost).to have_attributes(title: title, body: body, price: price)
+    end
+  end
 end
