773b2e3a6f5c47fe0821be22e62d9b10a55e8681
Ann Novakowski <annhnova@gmail.com>
2016-06-27 16:53:10 -0500

    Add validation to Topic + Refactor Topic with form_group_tag

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,12 +5,5 @@ class Post < ActiveRecord::Base
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
   validates :topic, presence: true
-  # def Post.write_spam!
-  #   Post.all.each_with_index do | post, index |
-  #     if index % 5 == 0 then
-  #       post.title = "SPAM"
-  #       post.save
-  #     end
-  #   end
-  # end
+
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,9 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
   #note: Because comments already depend on posts, they will also be deleted when a topic is deleted.
+
+  # checkpoint 35 - validate topic
+  validates :name, length: { minimum: 5 }, presence: true
+  validates :description, length: { minimum: 15 }, presence: true
+  
 end
