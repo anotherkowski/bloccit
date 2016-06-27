d592bd7522b9f8efd33cb6a35bdf5b6c9a7641f5
Ann Novakowski <annhnova@gmail.com>
2016-06-27 11:29:39 -0500

    Assignment 34 in progress

--- app/controllers/sponsored_posts_controller.rb
+++ app/controllers/sponsored_posts_controller.rb
@@ -3,12 +3,45 @@ class SponsoredPostsController < ApplicationController
     @sponsored_post = SponsoredPost.find(params[:id])
   end
 
-  # def new
-  #   @topic = Topic.find(params[:topic_id])
-  #   @sponsored_post = SponsoredPost.new
-  # end
+  def new
+    @topic = Topic.find(params[:topic_id])
+    @sponsored_post = SponsoredPost.new
+  end
 
-  # def edit
-  # end
+  def create
+    @sponsored_post = SponsoredPost.new
+    @sponsored_post.title = params[:sponsored_post][:title]
+    @sponsored_post.body = params[:sponsored_post][:body]
+    @sponsored_post.price = params[:sponsored_post][:price]
+    @sponsored_post = Topic.find(params[:topic_id])
+    @sponsored_post.topic = @topic
+
+    if @sponsored_post.save
+      flash[:notice] = "Sponsored Post was saved successfully"
+      redirect_to [@topic, @sponsored_post]
+    else
+      flash.now[:alert]
+      reder :new
+    end
+  end
+
+  def edit
+    @sponsored_post = SponsoredPost.find(params[:id])
+  end
+
+  def update
+    @sponsored_post = SponsoredPost.find(params[:id])
+    @sponsored_post.title = params[:sponsored_post][:title]
+    @sponsored_post.body = params[:sponsored_post][:body]
+    @sponsored_post.price = params[:sponsored_post][:price]
+
+    if @sponsored_post.save
+      flash[:notice] = "SponsoredPost was updated successfully"
+      redirect_to [@sponsored_post.topic, @sponsored_post]
+    else
+      flash.now[:alert] = "There was an error saving the post. Please try again."
+      render :edit
+    end
+  end
 
 end
--- spec/controllers/sponsored_posts_controller_spec.rb
+++ spec/controllers/sponsored_posts_controller_spec.rb
@@ -19,19 +19,74 @@ RSpec.describe SponsoredPostsController, type: :controller do
     end
   end
 
-  # describe "GET new" do
-  #   it "returns http success" do
-  #     get :new, topic_id: my_topic.id
-  #     expect(response).to have_http_status(:success)
-  #   end
-  #   it "renders the new view" do
-  #     get :new, topic_id: my_topic.id
-  #     expect(response).to render_template :new
-  #   end
-  #   it "instantiates @sponsored_post" do
-  #     get :new, topic_id: my_topic.id
-  #     expect(assigns(:sponsored_post)).not_to be_nil
-  #   end
-  # end
+  describe "GET new" do
+    it "returns http success" do
+      get :new, topic_id: my_topic.id
+      expect(response).to have_http_status(:success)
+    end
+    it "renders the new view" do
+      get :new, topic_id: my_topic.id
+      expect(response).to render_template :new
+    end
+    it "instantiates @sponsored_post" do
+      get :new, topic_id: my_topic.id
+      expect(assigns(:sponsored_post)).not_to be_nil
+    end
+  end
+
+  describe "POST create" do
+    it "increases the number of the sponsored posts by 1" do
+      expect{post :create, topic_id: my_topic.id, sponsored_post: {title: RandomData.random_sentence, body: RandomData.random_paragraph, price: rand(100)}}.to change(SponsoredPost,:count).by(1)
+    end
+    it "assigns the new sponsored post to @sponsored_post" do
+      post :create, topic_id: my_topic.id, sponsored_post: {title: RandomData.random_sentence, body: RandomData.random_paragraph, price: rand(100)}
+      expect(assigns(:sponsored_post)).to eq SponsoredPost.last
+    end
+    it "redirects to the new sponsored post" do
+      post :create, topic_id: my_topic.id, sponsored_post: {title: RandomData.random_sentence, body: RandomData.random_paragraph}
+      expect(response).to redirect_to [my_topic, SponsoredPost.last]
+    end
+  end
+
+  describe "GET #edit" do
+    it "returns http success" do
+      get :edit, topic_id: my_topic.id, id: my_sponsoredpost.id
+      expect(response).to have_http_status(:success)
+    end
+    it "renders the #edit view" do
+      get :edit, topic_id: my_topic.id, id: my_sponsoredpost.id
+      expect(response).to render_template :edit
+    end
+    it "assigns post to be updated to @post" do
+      get :edit, topic_id: my_topic.id, id: my_sponsoredpost.id
+      post_instance = assigns(:sponsored_post)
+
+      expect(post_instance.id).to eq my_sponsoredpost.id
+      expect(post_instance.title).to eq my_sponsoredpost.title
+      expect(post_instance.body).to eq my_sponsoredpost.body
+    end
+  end
+
+  describe "PUT update" do
+    it "updates post with expected attributes" do
+      new_title = RandomData.random_sentence
+      new_body = RandomData.random_paragraph
+
+      put :update, topic_id: my_topic.id, id: my_sponsoredpost.id, sponsored_post: {title: new_title, body: new_body}
+
+      updated_post = assigns(:sponsored_post)
+      expect(updated_post.id).to eq my_sponsoredpost.id
+      expect(updated_post.title).to eq new_title
+      expect(updated_post.body).to eq new_body
+    end
+
+    it "redirects to the updated post" do
+      new_title = RandomData.random_sentence
+      new_body = RandomData.random_paragraph
+
+      put :update, topic_id: my_topic.id, id: my_sponsoredpost.id, sponsored_post: {title: new_title, body: new_body}
+      expect(response).to redirect_to [my_topic, my_sponsoredpost]
+    end
+  end
 
 end
