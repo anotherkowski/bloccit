fdb98ef0014380c16bb069f1009f92137ca1af52
Ann Novakowski <annhnova@gmail.com>
2016-06-27 11:30:54 -0500

    Assignment 34 in progress

--- app/controllers/sponsored_posts_controller.rb
+++ app/controllers/sponsored_posts_controller.rb
@@ -1,6 +1,6 @@
 class SponsoredPostsController < ApplicationController
   def show
-    @sponsored_post = SponsoredPost.find(params[:id])
+    @sponsored_posts = SponsoredPost.find(params[:id])
   end
 
   def new
