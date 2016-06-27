582873afc02c5f6a3280e025dc21e16ccf7ef6cc
Ann Novakowski <annhnova@gmail.com>
2016-06-26 21:27:33 -0500

    Add #show view to SponsoredPosts controller

--- app/controllers/sponsored_posts_controller.rb
+++ app/controllers/sponsored_posts_controller.rb
@@ -1,2 +1,14 @@
 class SponsoredPostsController < ApplicationController
+  def show
+    @sponsored_post = SponsoredPost.find(params[:id])
+  end
+
+  # def new
+  #   @topic = Topic.find(params[:topic_id])
+  #   @sponsored_post = SponsoredPost.new
+  # end
+
+  # def edit
+  # end
+
 end
--- spec/controllers/sponsored_posts_controller_spec.rb
+++ spec/controllers/sponsored_posts_controller_spec.rb
@@ -4,19 +4,34 @@ RSpec.describe SponsoredPostsController, type: :controller do
   let(:my_topic) { Topic.create!(name:  RandomData.random_sentence, description: RandomData.random_paragraph) }
   let(:my_sponsoredpost) { my_topic.sponsored_posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, price: rand(100)) }
 
-  describe "GET new" do
-    it "returns http success" do
-      get :new, topic_id: my_topic.id
+  describe "GET show" do
+    it "returns HTTP success" do
+      get :show, topic_id: my_topic.id, id: my_sponsoredpost.id
       expect(response).to have_http_status(:success)
     end
-    it "renders the new view" do
-      get :new, topic_id: my_topic.id
-      expect(response).to render_template :new
+    it "renders the #show view" do
+      get :show, topic_id: my_topic.id, id: my_sponsoredpost.id
+      expect(response).to render_template :show
     end
-    it "instantiates @sponsored_post" do
-      get :new, topic_id: my_topic.id
-      expect(assigns(:sponsored_post)).not_to be_nil
+    it "assigns my_sponsoredpost to @sponsored_post" do
+      get :show, topic_id: my_topic.id, id: my_sponsoredpost.id
+      expect(assigns(:sponsored_post)).to eq(my_sponsoredpost)
     end
   end
+
+  # describe "GET new" do
+  #   it "returns http success" do
+  #     get :new, topic_id: my_topic.id
+  #     expect(response).to have_http_status(:success)
+  #   end
+  #   it "renders the new view" do
+  #     get :new, topic_id: my_topic.id
+  #     expect(response).to render_template :new
+  #   end
+  #   it "instantiates @sponsored_post" do
+  #     get :new, topic_id: my_topic.id
+  #     expect(assigns(:sponsored_post)).not_to be_nil
+  #   end
+  # end
 
 end
