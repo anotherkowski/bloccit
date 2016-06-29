49dba792e7e3b65b7fa63b255efbf67bd6a0a056
Ann Novakowski <annhnova@gmail.com>
2016-06-29 14:17:59 -0500

    Add User routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :topics do
     resources :posts, except: [:index]
   end
+  resources :users, only: [:new, :create]
 
   get 'about' => 'welcome#about'
 
