6302c4176d4e99432f989c8a2b0aef34370b4969
Ann Novakowski <annhnova@gmail.com>
2016-06-29 15:12:18 -0500

    Create new User spec for name validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   before_save {self.email = email.downcase if email.present?}
+
   # Validate name
   validates :name, length: {minimum: 1, maximum: 100}, presence: true
   # Validate password
@@ -12,4 +13,6 @@ class User < ActiveRecord::Base
             length: {minimum: 3, maximum: 254}
 
     has_secure_password
+
+    
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -32,4 +32,18 @@ RSpec.describe User, type: :model do
       expect(user_with_invalid_email).to_not be_valid
     end
   end
+
+  describe "user name" do
+    let(:user_with_lowercase_name) {User.new(name: "ann novakowski", email: "ann@annhnova.com")}
+
+    it "has a space between first and last name" do
+      it {should include(" ")}
+    end
+    it "has capitalized first name" do
+      expect(user_with_lowercase_name.name.split(" ")[0]).to eq("Ann")
+    end
+    it "has capitalized last name" do
+      expect(user_with_lowercase_name.name.split(" ")[1]).to eq("Novakowski")
+    end
+  end
 end
