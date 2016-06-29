5368f64b56f6f1c39a6de8a7cd75bda3e6be595d
Ann Novakowski <annhnova@gmail.com>
2016-06-29 15:38:51 -0500

    Checkpoint 36: completed

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,6 +15,7 @@ RSpec.describe User, type: :model do
   it {is_expected.to have_secure_password}
   it {is_expected.to validate_length_of(:password).is_at_least(6)}
 
+
   describe "attributes" do
     it "should have name and email attributes" do
       expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com")
