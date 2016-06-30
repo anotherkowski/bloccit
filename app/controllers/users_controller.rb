77fccfdab16375a22f8993c983d581fd6ce31c9b
Ann Novakowski <annhnova@gmail.com>
2016-06-30 12:28:56 -0500

    Debugging - stuck :/

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,15 +23,15 @@ class UsersController < ApplicationController
     @user = User.new
     @user.name = params[:user][:name]
     @user.email = params[:user][:email]
-    # @user.password = params[:user][:password]
-    # @user.password_confirmation = params[:user][:password_confirmation]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
 
     if @user.save
       flash[:notice] = "Welcome to Bloccit #{@user.name}!"
-      redirect_to create_path
+      redirect_to root_path
     else
       flash.now[:alert] = "There was an error creating your account. Please try again."
-      render :create
+      render :new
     end
   end
 end
