a1f0045d25a55aea35bd259d5ee24c1b528fca1c
Ann Novakowski <annhnova@gmail.com>
2016-07-05 10:14:16 -0500

    Create SessionsHelper and update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
     resources :posts, except: [:index]
   end
   resources :users, only: [:new, :create]
-
+  resources :sessions, only: [:new, :create, :destroy]
+  
   get 'about' => 'welcome#about'
 
   root 'welcome#index'
