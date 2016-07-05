0bb154295867b62d469fdabbd2ca2a83ad4617c2
Ann Novakowski <annhnova@gmail.com>
2016-07-04 21:24:03 -0500

    Write tests for SessionsController

---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+RSpec.describe SessionsController, type: :controller do
+  let(:my_user) {User.create!(name: "Blochead", email: "blochead@bloc.io", password: "password")}
+
+  describe "GET new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "POST sessions" do
+    it "returns http success" do
+      post :create, session: {email: my_user.email}
+      expect(response).to have_http_status(:success)
+    end
+    it "initializes a session" do
+      post :create, session: {email: my_user.email, password: my_user.password}
+      expect(session[:user_id]).to eq my_user.id
+    end
+    it "flashes #error with bad email address" do
+      post :create, session: {email: "does not exist"}
+      expect(flash.now[:alert]).to be_present
+    end
+    it "renders #new with bad email address" do
+      post :create, session: {email: "does not exist"}
+      expect(response).to render_template :new
+    end
+    it "redirects to the root view" do
+      post :create, session: {email: my_user.email, password: my_user.password}
+      expect(response).to redirect_to(root_path)
+    end
+    it "sets user password_confirmation properly" do
+       post :create, user: new_user_attributes
+       expect(assigns(:user).password_confirmation).to eq new_user_attributes[:password_confirmation]
+     end
+     it "logs the user in after sign up" do
+       post :create, user: new_user_attributes
+       expect(session[:user_id]).to eq assigns(:user).id
+     end
+  end
+
+  describe "DELETE sessions/id" do
+    it "render the #welcome view" do
+      delete :destroy, id: my_user.id
+      expect(response).to redirect_to root_path
+    end
+    it "deletes the user's session" do
+      delete :destroy, id: my_user.id
+      expect(assigns(:session)).to be_nil
+    end
+    it "flashes #notice" do
+      delete :destroy, id: my_user.id
+      expect(flash[:notice]).to be_present
+    end
+  end
+
+end
