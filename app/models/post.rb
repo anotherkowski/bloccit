87fbf8f80bff1beaf3e6c4511d6704c821ae4d15
Ann Novakowski <annhnova@gmail.com>
2016-07-10 16:54:30 -0500

    assignment 39 - best attempt

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,11 +1,11 @@
 class Post < ActiveRecord::Base
   belongs_to :topic
-
   belongs_to :user
-
   has_many :comments, dependent: :destroy
 
-  default_scope { order('created_at DESC') } 
+  default_scope { order('created_at DESC') }
+  scope :ordered_by_title, -> { reorder(title: :asc) }
+  scope :ordered_by_reverse_created_at, -> { reorder('created_at ASC') }
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
