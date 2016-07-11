695f6da8aa910acf5c4d6a4c66aaea281dd899b7
Ann Novakowski <annhnova@gmail.com>
2016-07-10 22:20:04 -0500

    Checkpoint 40 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -44,11 +44,19 @@ posts = Post.all
   )
 end
 
-# Use my own email as the first test
-user = User.first
-user.update_attributes!(
-  email: 'annhnova@gmail.com',
-  password: 'password'
+# Create an admin user
+admin = User.create!(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+)
+
+# Create a member
+member = User.create!(
+  name:     'Member User',
+  email:    'member@example.com',
+  password: 'helloworld'
 )
 
 puts "Seed finished"
