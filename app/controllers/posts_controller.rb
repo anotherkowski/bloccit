e15460df39c5ecc4496297bd426be5c2e157f388
Ann Novakowski <annhnova@gmail.com>
2016-07-11 14:34:14 -0500

    Assignment 40: almost there... Moderators can still delete posts :(

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :require_sign_in, except: :show
-  before_action :authorize_user, except: [:show, :new, :create]
+  before_action :authorize_user, except: [:show, :new, :create, :delete]
+  before_action :authorize_delete, only: [:delete]
 
   def show
     @post = Post.find(params[:id])
@@ -64,12 +65,20 @@ class PostsController < ApplicationController
     params.require(:post).permit(:title, :body)
   end
 
+  def authorize_delete
+    post = Post.find(params[:id])
+    if current_user.moderator?
+      flash[:alert] = "Moderators cannot delete posts."
+      redirect_to [post.topic, post]
+    end
+  end
+
   def authorize_user
     post = Post.find(params[:id])
-    unless current_user == post.user || current_user.admin?
+    unless current_user == post.user || (current_user.moderator? || current_user.admin?)
       flash[:alert] = "You must be an admin to do that."
       redirect_to [post.topic, post]
-      end
+    end
   end
 
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,9 @@
 module PostsHelper
   def user_is_authorized_for_post?(post)
-        current_user && (current_user == post.user || current_user.admin?)
-   end
+    current_user && (current_user == post.user || current_user.moderator? || current_user.admin?)
+  end
+
+  def user_can_delete?(post)
+    current_user && (current_user == post.user || current_user.admin?)
+  end
 end
