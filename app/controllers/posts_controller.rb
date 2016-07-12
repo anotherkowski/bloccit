9a1bd5ea8acda4947137f621ce8b0d67094f7c57
Ann Novakowski <annhnova@gmail.com>
2016-07-12 12:19:47 -0500

    Debugging...

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :require_sign_in, except: :show
+  before_action :require_sign_in, except: [:show]
   before_action :authorize_user, except: [:show, :new, :create]
 
   def show
