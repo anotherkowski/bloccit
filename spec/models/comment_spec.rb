7cec8168a3b8912add627cc7d86fa87c6200bfb2
Ann Novakowski <annhnova@gmail.com>
2016-07-13 20:33:00 -0500

    Update post, topic, and comment controllers for commentable assoc

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,10 +4,11 @@ RSpec.describe Comment, type: :model do
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld")}
   let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user)}
-  let(:comment) { Comment.create!(body: 'Comment Body', commentable: post, user: user) }
+  let(:comment) { Comment.create!(body: "Comment Body", commentable: post, user: user) }
 
   it { is_expected.to belong_to(:commentable) }
   it { is_expected.to belong_to(:user) }
+
   # test that the comment's body exists and has a minimum length
   it { is_expected.to validate_presence_of(:body) }
   it { is_expected.to validate_length_of(:body).is_at_least(5) }
