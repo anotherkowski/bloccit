fe368a7fa33aeccc5da945c261db13ab502f6764
Ann Novakowski <annhnova@gmail.com>
2016-07-13 19:37:54 -0500

    Update Comment model spec for commentable

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,23 +7,9 @@ class CommentsController < ApplicationController
     @comment = @commentable.comments.build(comment_params)
   end
 
-  # def create
-  #   @post = Post.find(params[:post_id])
-  #   comment = @post.comments.new(comment_params)
-  #   comment.user = current_user
-  #
-  #   if comment.save
-  #     flash[:notice] = "Successfully created..."
-  #     redirect_to [@post.topic, @post]
-  #   else
-  #     flash[:alert] = "Comment failed to save."
-  #     redirect_to [@post.topic, @post]
-  #   end
-  # end
-
   def destroy
-    @commentable = find_commentable
-    @comment = @commentable.comments.build(comment_params)
+    @comment = set_comment
+    @commentable = @comment.commentable
     if @comment.destroy
        flash[:notice] = "Comment was deleted successfully."
        redirect_to [@commentable]
@@ -36,12 +22,12 @@ class CommentsController < ApplicationController
   private
 
   def find_commentable
-    params.each do |name, value|
-      if name =~ /(.+)_id$/
-        return $1.classify.constantize.find(value)
-      end
-    end
-    nil
+    resource, id = request.path.split('/')[1, 2]
+    @commentable = resource.singularize.classify.constantize.find(id)
+  end
+
+  def set_comment
+    Comment.find(params[:id])
   end
 
   def comment_params
