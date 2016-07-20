71c56cc4176ed7b0d8565902d5807aa8eb1ac9b1
Ann Novakowski <annhnova@gmail.com>
2016-07-19 20:36:49 -0500

    Begin checkpoint 43

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160712182632) do
+ActiveRecord::Schema.define(version: 20160720013516) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -80,5 +80,16 @@ ActiveRecord::Schema.define(version: 20160712182632) do
     t.datetime "updated_at",      null: false
     t.integer  "role"
   end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "value"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "votes", ["post_id"], name: "index_votes_on_post_id"
+  add_index "votes", ["user_id"], name: "index_votes_on_user_id"
 
 end
