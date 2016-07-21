21b263f0ce3664d2d1c26177ad0a05b8a16bac8e
Ann Novakowski <annhnova@gmail.com>
2016-07-21 13:20:02 -0500

    Checkpoint 43: completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160720013516) do
+ActiveRecord::Schema.define(version: 20160721174945) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20160720013516) do
     t.datetime "updated_at", null: false
     t.integer  "topic_id"
     t.integer  "user_id"
+    t.float    "rank"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
