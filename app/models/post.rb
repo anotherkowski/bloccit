4d9a1c4145efa7569b33efca64be8bed7c1a5d32
Ann Novakowski <annhnova@gmail.com>
2016-07-21 10:13:07 -0500

    Implementing the UI for votes

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,7 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   has_many :labelings, as: :labelable
   has_many :labels, through: :labelings
+  has_many :votes, dependent: :destroy
 
   default_scope { order('created_at DESC') }
 
@@ -12,4 +13,17 @@ class Post < ActiveRecord::Base
   validates :body, length: { minimum: 20 }, presence: true
   validates :topic, presence: true
   validates :user, presence: true
+
+  def up_votes
+    votes.where(value: 1).count
+  end
+
+  def down_votes
+    votes.where(value: -1).count
+  end
+
+  def points
+    votes.sum(:value)
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -27,12 +27,15 @@ topics = Topic.all
 
 # Create Posts
 50.times do
-  Post.create!( # Adding a ! instructs the method to raise an error if there's a problem with the data we're seeding.
+  post = Post.create!(
+   # Adding a ! instructs the method to raise an error if there's a problem with the data we're seeding.
   user: users.sample,
   topic: topics.sample,
   title: RandomData.random_sentence,
   body: RandomData.random_paragraph
   )
+  post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+  rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
 end
 posts = Post.all
 
@@ -65,3 +68,4 @@ puts "#{User.count} users created"
 puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Vote.count} votes created"
