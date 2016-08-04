cf4e8d054519c9c0ea61ae7c34e18e916fe8dcca
Ann Novakowski <annhnova@gmail.com>
2016-08-04 18:40:15 -0500

    Add method new_post to favorites mailer

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -1,5 +1,5 @@
 class FavoriteMailer < ApplicationMailer
-  default from: "annhnova@gmail.com"
+  default from: "bloccit@gmail.com"
 
   def new_comment(user, post, comment)
     headers["Message-ID"] = "<comments/#{comment.id}@your-app-name.example>"
@@ -11,5 +11,16 @@ class FavoriteMailer < ApplicationMailer
     @comment = comment
 
     mail(to: user.email, subect: "New comment on #{post.title}")
+  end
+
+  def new_post(user,post)
+    headers["Message-ID"] = "<topic/#{post.topic.id}@your-app-name.example>"
+    headers["In-Reply-To"] = "<post/#{post.id}@your-app-name.example>"
+    headers["References"] = "<post/#{post.id}@your-app-name.example>"
+
+    @user = user
+    @post = post
+
+    mail(to: user.email, cc: "annhnova@gmail.com", subject: "You created #{post.title}")
   end
 end
