741711fdd5f90f334663a7b1c23c1930854cd116
Ann Novakowski <annhnova@gmail.com>
2016-08-17 19:16:17 -0500

    Assignment 45 - Working, but not the way the assignment asked for it to work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,22 +25,4 @@ class UsersController < ApplicationController
     @posts = @user.posts.visible_to(current_user)
   end
 
-  private
-
-  def no_posts?(user)
-    @user = User.find(params[:id])
-    user.posts.count > 0
-  end
-  def no_posts_message(user)
-    @user = User.find(params[:id])
-    "#{user.name} has not submitted any posts yet.}"
-  end
-  def no_comments?(user)
-    @user = User.find(params[:id])
-    user.comments.count > 0
-  end
-  def no_comments_message(user)
-    @user = User.find(params[:id])
-    "#{user.name} has not submitted any comments yet.}"
-  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,14 +1,3 @@
 module UsersHelper
-  def no_posts?(user)
-    user.posts.count > 0
-  end
-  def no_posts_message(user)
-    "#{user.name} has not submitted any posts yet.}"
-  end
-  def no_comments?(user)
-    user.comments.count > 0
-  end
-  def no_comments_message(user)
-    "#{user.name} has not submitted any comments yet.}"
-  end
+  
 end
