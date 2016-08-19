e9389dfaec7835049bf4d4f07a9db502086d78a4
Ann Novakowski <annhnova@gmail.com>
2016-08-18 21:52:45 -0500

    Assignment 45 - Method for pt2 complete

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,12 @@
 module UsersHelper
-  def favorited_posts_for(user)
-    user.favorites.map{|favorite| favorite.post}
+  # def favorited_posts_for(user)
+  #   user.favorites.map{|favorite| favorite.post}
+  # end
+
+  def has_posts?(user)
+    user.posts.empty?
+  end
+  def has_comments?(user)
+    user.comments.empty?
   end
 end
